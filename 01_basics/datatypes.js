78317983ee7eb25c0ab47201d963e92753dad31b
Asmita Sharma <155943217+asmitasharma04@users.noreply.github.com>
2025-02-05 15:36:12 +0000

    commit

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,24 @@
+//"use strict";//treat all JS code as newer user...it is not required to write ..but it was used earlier;
+//alert(3+3)//it will show error when i will run it in terminal but will go correctly in console because we are using node JS
+
+//Code readability should be high 
+
+let name="asmita"//String data type
+let age=18//numeric
+let isloggedIn=false//boolean
+
+
+
+//number => 2 to power 53
+//bigint
+ //string=> " "
+ //boolean=> true/false
+ //null=> standalone value ...it is a representation of empty value e.g let h=""; or let h=null;
+ //undefined=> variable is defined but no value is assigned
+ //symbol=> uniqueness ke liye use hota hai
+
+ //object
+
+ console.log(typeof "asmita");
+ console.log(typeof null);  //type is object
+ console.log(typeof undefined);  //type is undefined
